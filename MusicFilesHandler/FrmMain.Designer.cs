0a3af2c2833679e4eb3b08b50ce9494d96c851a8
孫氷 <sunbingms@hotmail.com>
2020-11-20 10:33:50 +0900

    プロジェクト ファイルを追加します。

---
+++ MusicFilesHandler/FrmMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace MusicFilesHandler
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbRootPath = new System.Windows.Forms.TextBox();
+            this.btnBrower = new System.Windows.Forms.Button();
+            this.fbdFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Music Folder:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStart.Location = new System.Drawing.Point(16, 84);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(468, 48);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbRootPath
+            // 
+            this.tbRootPath.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbRootPath.Location = new System.Drawing.Point(130, 26);
+            this.tbRootPath.Name = "tbRootPath";
+            this.tbRootPath.Size = new System.Drawing.Size(258, 29);
+            this.tbRootPath.TabIndex = 2;
+            // 
+            // btnBrower
+            // 
+            this.btnBrower.Location = new System.Drawing.Point(394, 26);
+            this.btnBrower.Name = "btnBrower";
+            this.btnBrower.Size = new System.Drawing.Size(90, 29);
+            this.btnBrower.TabIndex = 3;
+            this.btnBrower.Text = "Brower...";
+            this.btnBrower.UseVisualStyleBackColor = true;
+            this.btnBrower.Click += new System.EventHandler(this.btnBrower_Click);
+            // 
+            // fbdFolder
+            // 
+            this.fbdFolder.Description = "Select your music folder";
+            this.fbdFolder.ShowNewFolderButton = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 144);
+            this.Controls.Add(this.btnBrower);
+            this.Controls.Add(this.tbRootPath);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Music Folder Maid";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbRootPath;
+        private System.Windows.Forms.Button btnBrower;
+        private System.Windows.Forms.FolderBrowserDialog fbdFolder;
+    }
+}
+
---
+++ MusicFilesHandler/FrmMain.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Logic;
+using System.IO;
+
+namespace MusicFilesHandler
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 開始ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string rootPath = this.tbRootPath.Text.Trim();
+            MainLogic mainLogic = new MainLogic();
+            if (mainLogic.isPathExist(rootPath))
+            {
+                try
+                {
+                    List<string> listFile = new List<string>();
+                    DirectoryInfo directory = new DirectoryInfo(rootPath);
+                    mainLogic.findFile(listFile, directory);
+                    int num = 0;
+                    foreach(string fileName in listFile)
+                    {
+                        if (mainLogic.isMusicFile(fileName))
+                        {
+                            string targetPath = rootPath;
+                            string artist = mainLogic.getArtist(fileName);
+                            string album = mainLogic.getAlbum(fileName);
+                            if (!string.IsNullOrEmpty(artist) && !mainLogic.isAllQuestionMark(artist))
+                            {
+                                targetPath += "\\" + artist;
+                            }
+                            if (!string.IsNullOrEmpty(album) && !mainLogic.isAllQuestionMark(album))
+                            {
+                                targetPath += "\\" + album;
+                            }
+                            mainLogic.moveFile(fileName, targetPath);
+                            num++;
+                        }
+                    }
+                    showSuccessMessage("移动完成,本次处理文件" + num + "个");
+                }
+                catch (Exception ex)
+                {
+                    showErrorMessage(ex.Message);
+                }
+            }
+            else
+            {
+                showErrorMessage("输入的路径不存在，请确认后操作");
+            }
+        }
+
+        /// <summary>
+        /// showErrorMessage
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        private void showErrorMessage(string errorMessage)
+        {
+            MessageBox.Show(errorMessage, "出错啦", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// showSuccessMessage
+        /// </summary>
+        /// <param name="successMessage"></param>
+        private void showSuccessMessage(string successMessage)
+        {
+            MessageBox.Show(successMessage, "成功了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// 参照ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBrower_Click(object sender, EventArgs e)
+        {
+            //ダイアログを表示する
+            if (fbdFolder.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                tbRootPath.Text = fbdFolder.SelectedPath;
+            }
+        }
+    }
+}
